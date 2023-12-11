7f91238cbb6269bdcf0d332b91904f8e03de4c54
git config --global user.name tamzid620 <tamzid620@gmail.com>
2023-12-11 13:04:12 +0600

    reservation paragraph logic added

--- src/Components/Shared/Reservation/Reservation.jsx
+++ src/Components/Shared/Reservation/Reservation.jsx
@@ -1,6 +1,6 @@
 import aboutPhoto from "../../../../public/images/contactUs.jpg";
 import hrIcon from "../../../../public/icons/hr.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
 
@@ -16,6 +16,7 @@ const Reservation = () => {
     numbOfPeople: "",
     eventInfo: "",
   });
+  const [showMessage, setShowMessage] = useState([]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -29,41 +30,55 @@ const Reservation = () => {
     console.log("Form Data Submitted:", formData);
 
     // post method ---------------------
-  axios.post("https://backend.ap.loclx.io/api/add-reservation", formData)
-    .then((res) => {
-      console.log("Response from the server:", res.data);
-      Swal.fire({
-        position: "center",
-        icon: "success",
-        title: res.data.message,
-        showConfirmButton: false,
-        timer: 1500
+    axios
+      .post("https://backend.ap.loclx.io/api/add-reservation", formData)
+      .then((res) => {
+        console.log("Response from the server:", res.data);
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: res.data.message,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+
+        // Reset the form
+        setFormData({
+          clientName: "",
+          email: "",
+          phoneNo: "",
+          eventDate: "",
+          startTime: "",
+          endTime: "",
+          eventType: "",
+          numbOfPeople: "",
+          eventInfo: "",
+        });
+      })
+      .catch((error) => {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: console.error("Error submitting form:", error),
+          showConfirmButton: false,
+          timer: 1500,
+        });
       });
-      
-      // Reset the form
-      setFormData({
-        clientName: "",
-        email: "",
-        phoneNo: "",
-        eventDate: "",
-        startTime: "",
-        endTime: "",
-        eventType: "",
-        numbOfPeople: "",
-        eventInfo: "",
-      });
-    })
-    .catch((error) => {
-      
-      Swal.fire({
-        position: "center",
-        icon: "error",
-        title: console.error("Error submitting form:", error),
-        showConfirmButton: false,
-        timer: 1500
-      });
-    });
   };
+
+  useEffect(() => {
+    // get method -------------------
+    axios
+      .get("https://backend.ap.loclx.io/api/user-reservation-info")
+      .then((res) => {
+        setShowMessage(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching reservation info:", error);
+
+        console.log(showMessage);
+      });
+  }, [showMessage]);
 
   return (
     <div>
@@ -91,94 +106,120 @@ const Reservation = () => {
 
       {/* information section */}
       <div className="mx-auto mt-8 p-4 max-w-full ">
+        
+           {/* show message  */}
+    {showMessage.status === "201" && (
+      <div className="flex justify-center text-lg text-green-500">
+        <div>
+          <p className="flex justify-center">{showMessage.message}</p>
+          {showMessage.reserve?.eventDate && (
+            <p className="flex justify-center">
+              Date: {showMessage.reserve.eventDate}
+            </p>
+          )}
+          <p className="flex justify-center">
+            <span className="me-2">
+              Time: {showMessage.reserve?.startTime}
+            </span>{" "}
+            to <span className="ms-2">{showMessage.reserve?.endTime}</span>
+          </p>
+        </div>
+      </div>
+    )}
+
+    {showMessage.status === "202" && (
+      <p className="flex justify-center text-lg text-green-500">{showMessage.message}</p>
+    )}
+
+    {showMessage.status === "401" && (
+      <p className="flex justify-center text-lg text-green-500">{showMessage.message}</p>
+    )}
+        {/* form section  */}
         <form
           onSubmit={handleSubmit}
           className=" shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-transparent"
         >
-              <div className="mb-4 grid grid-cols-2 gap-4">
+          <div className="mb-4 grid grid-cols-2 gap-4">
+            {/* Name Input */}
+            <div className="mb-4">
+              <label
+                className="block text-white text-sm font-bold mb-2"
+                htmlFor="clientName"
+              >
+                Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="clientName"
+                type="text"
+                placeholder="Full Name"
+                name="clientName"
+                value={formData.clientName}
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-          {/* Name Input */}
-          <div className="mb-4">
-            <label
-              className="block text-white text-sm font-bold mb-2"
-              htmlFor="clientName"
-            >
-              Name
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="clientName"
-              type="text"
-              placeholder="Full Name"
-              name="clientName"
-              value={formData.clientName}
-              onChange={handleChange}
-              required
-            />
+            {/*Email Input */}
+            <div className="mb-4">
+              <label
+                className="block text-white text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="text"
+                placeholder="Your Email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
           </div>
-
-          {/*Email Input */}
-          <div className="mb-4">
-            <label
-              className="block text-white text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="text"
-              placeholder="Your Email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-
-              </div>
 
           <div className="mb-4 grid grid-cols-2 gap-4">
+            {/* Phone Number Input */}
+            <div className="mb-4">
+              <label
+                className="block  text-white text-sm font-bold mb-2"
+                htmlFor="phoneNo"
+              >
+                Phone Number
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="phoneNo"
+                type="tel"
+                placeholder="Phone Number"
+                name="phoneNo"
+                value={formData.phoneNo}
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-          {/* Phone Number Input */}
-          <div className="mb-4">
-            <label
-              className="block  text-white text-sm font-bold mb-2"
-              htmlFor="phoneNo"
-            >
-              Phone Number
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="phoneNo"
-              type="tel"
-              placeholder="Phone Number"
-              name="phoneNo"
-              value={formData.phoneNo}
-              onChange={handleChange}
-              required
-            />
-          </div>
-
-          {/* Event Date Input */}
-          <div className="mb-4">
-            <label
-              className="block  text-white text-sm font-bold mb-2"
-              htmlFor="eventDate"
-            >
-              Event Date
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="eventDate"
-              type="date"
-              name="eventDate"
-              value={formData.eventDate}
-              onChange={handleChange}
-              required
-            />
-          </div>
+            {/* Event Date Input */}
+            <div className="mb-4">
+              <label
+                className="block  text-white text-sm font-bold mb-2"
+                htmlFor="eventDate"
+              >
+                Event Date
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="eventDate"
+                type="date"
+                name="eventDate"
+                value={formData.eventDate}
+                onChange={handleChange}
+                required
+              />
+            </div>
           </div>
 
           {/* StartTime and EndTime Inputs */}
@@ -220,54 +261,53 @@ const Reservation = () => {
           </div>
 
           <div className="mb-4 grid grid-cols-2 gap-4">
+            {/* Type of Event Selector */}
+            <div className="mb-4">
+              <label
+                className="block  text-white text-sm font-bold mb-2"
+                htmlFor="eventType"
+              >
+                Type of Event
+              </label>
+              <select
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="eventType"
+                name="eventType"
+                value={formData.eventType}
+                onChange={handleChange}
+                required
+              >
+                <option value="" disabled>
+                  Select Type
+                </option>
+                <option value="birthday">Corporate Lunch</option>
+                <option value="wedding">Family Event</option>
+                <option value="wedding">Meeting / Presentation</option>
+                <option value="wedding">Wedding</option>
+                <option value="wedding">Birthday party</option>
+                <option value="wedding">Other</option>
+                {/* Add more options as needed */}
+              </select>
+            </div>
 
-          {/* Type of Event Selector */}
-          <div className="mb-4">
-            <label
-              className="block  text-white text-sm font-bold mb-2"
-              htmlFor="eventType"
-            >
-              Type of Event
-            </label>
-            <select
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="eventType"
-              name="eventType"
-              value={formData.eventType}
-              onChange={handleChange}
-              required
-            >
-              <option value="" disabled>
-                Select Type
-              </option>
-              <option value="birthday">Corporate Lunch</option>
-              <option value="wedding">Family Event</option>
-              <option value="wedding">Meeting / Presentation</option>
-              <option value="wedding">Wedding</option>
-              <option value="wedding">Birthday party</option>
-              <option value="wedding">Other</option>
-              {/* Add more options as needed */}
-            </select>
-          </div>
-
-          {/* Number of People Input */}
-          <div className="mb-4">
-            <label
-              className="block  text-white text-sm font-bold mb-2"
-              htmlFor="numbOfPeople"
-            >
-              Number of People
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="numbOfPeople"
-              type="number"
-              name="numbOfPeople"
-              value={formData.numbOfPeople}
-              onChange={handleChange}
-              required
-            />
-          </div>
+            {/* Number of People Input */}
+            <div className="mb-4">
+              <label
+                className="block  text-white text-sm font-bold mb-2"
+                htmlFor="numbOfPeople"
+              >
+                Number of People
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="numbOfPeople"
+                type="number"
+                name="numbOfPeople"
+                value={formData.numbOfPeople}
+                onChange={handleChange}
+                required
+              />
+            </div>
           </div>
 
           {/* Additional Information Input */}
