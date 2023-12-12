738845967f311681bb5eedb763c1c733b35ce005
git config --global user.name tamzid620 <tamzid620@gmail.com>
2023-12-12 11:26:49 +0600

    online order slider adde to the other cart as row

--- src/Components/Shared/OnlineOrder/OnlineOrder.jsx
+++ src/Components/Shared/OnlineOrder/OnlineOrder.jsx
@@ -20,7 +20,47 @@ import "aos/dist/aos.css";
 
 const OnlineOrder = () => {
   Aos.init();
-  const settings = {
+  const settings0 = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const settings1 = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const settings2 = {
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
@@ -72,9 +112,7 @@ const OnlineOrder = () => {
       <div className="flex justify-center" data-aos="zoom-in-up">
         <div className="max-w-screen-lg mx-auto ">
           {/*---------------------row -1 ---------------- */}
-
-          <Slider className="mt-10 lg:ms-0 lg:me-0 md:ms-[150px] md:me-[150px] sm: ms-[350px] sm: me-[350px]" {...settings}>
-          {/* <div className="mt-10 gap-1o grid sm: grid-cols-1 md:grid-cols-2 lg:grid-cols-3"> */}
+          <Slider className="mt-10 lg:ms-0 lg:me-0 md:ms-[150px] md:me-[150px] sm: ms-[350px] sm: me-[350px]"  {...settings0}>
             {/* food item -1 */}
             <div
               data-aos="flip-up"
@@ -197,7 +235,7 @@ const OnlineOrder = () => {
                 </button>
               </div>
             </div>
-            {/* food item -3 */}
+            {/* food item -4 */}
             <div
               data-aos="flip-up"
               className=" rounded-xl  border-t-2 border-yellow-500 shadow-lg shadow-yellow-500 p-3 w-80 transition-transform transform hover:scale-105"
@@ -237,11 +275,10 @@ const OnlineOrder = () => {
                 </button>
               </div>
             </div>
-          {/* </div> */}
             </Slider>
 
           {/*---------------------row -2 ---------------- */}
-          <div className="mt-10 gap-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <Slider className="mt-10  lg:ms-0 lg:me-0 md:ms-[150px] md:me-[150px] sm: ms-[350px] sm: me-[350px]"  {...settings1}>
             {/* food item -4 */}
             <div
               data-aos="flip-up"
@@ -364,10 +401,50 @@ const OnlineOrder = () => {
                 </button>
               </div>
             </div>
-          </div>
+            {/* food item -7 */}
+            <div
+              data-aos="flip-up"
+              className=" rounded-xl border-t-2 border-yellow-500 shadow-lg shadow-yellow-500 p-3 w-80 transition-transform transform hover:scale-105"
+            >
+              <img className="w-80 h-48 rounded-lg" src={photo2} alt="" />
+              <div>
+                <h1 className=" text-xl text-white font-semibold uppercase mt-5">
+                  Bibimbap
+                </h1>
+                <h2 className="text-[#808080] font-semibold">
+                  Lorem ipsum dolor sit amet consectetur <br /> adipisicing
+                  elit. Iusto, vitae?
+                </h2>
+                <div className="flex justify-between mt-5">
+                  <p className="text-yellow-500 text-2xl font-mono font-bold">
+                    $20.00
+                  </p>
+                  <div className="flex">
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon2} alt="" />
+                  </div>
+                </div>
+                <button
+                  onClick={handleOrderNowClick}
+                  className="hover:bg-[#FFD700] hover:text-black
+                             bg-[#FFD700]  text-[#808080] border-black font-bold px-2  py-1 
+                             rounded-md mt-3 flex items-center gap-2"
+                >
+                  <span> Add to Cart </span>
+                  <span>
+                    {" "}
+                    <BsFillCartPlusFill />{" "}
+                  </span>
+                </button>
+              </div>
+            </div>
+          </Slider>
 
           {/*---------------------row -3 ---------------- */}
-          <div className="mt-10 gap-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <Slider className="mt-10  lg:ms-0 lg:me-0 md:ms-[150px] md:me-[150px] sm: ms-[350px] sm: me-[350px]"  {...settings2}>
             {/* food item -7 */}
             <div
               data-aos="flip-up"
@@ -490,7 +567,213 @@ const OnlineOrder = () => {
                 </button>
               </div>
             </div>
-          </div>
+            {/* food item -10 */}
+            <div
+              data-aos="flip-up"
+              className=" rounded-xl border-t-2 border-yellow-500 shadow-lg shadow-yellow-500 p-3 w-80 transition-transform transform hover:scale-105"
+            >
+              <img className="w-80 h-48 rounded-lg" src={photo1} alt="" />
+              <div>
+                <h1 className=" text-xl text-white font-semibold uppercase mt-5">
+                  Beef Wellington
+                </h1>
+                <h2 className="text-[#808080] font-semibold">
+                  Lorem ipsum dolor sit amet consectetur <br /> adipisicing
+                  elit. Iusto, vitae?
+                </h2>
+                <div className="flex justify-between mt-5">
+                  <p className="text-yellow-500 text-2xl font-mono font-bold">
+                    $22.50
+                  </p>
+                  <div className="flex">
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon2} alt="" />
+                  </div>
+                </div>
+                <button
+                  onClick={handleOrderNowClick}
+                  className="hover:bg-[#FFD700] hover:text-black
+                             bg-[#FFD700]  text-[#808080] border-black font-bold px-2  py-1 
+                             rounded-md mt-3 flex items-center gap-2"
+                >
+                  <span> Add to Cart </span>
+                  <span>
+                    {" "}
+                    <BsFillCartPlusFill />{" "}
+                  </span>
+                </button>
+              </div>
+            </div>
+          </Slider>
+
+          {/*---------------------row -4 ---------------- */}
+          <Slider className="mt-10  lg:ms-0 lg:me-0 md:ms-[150px] md:me-[150px] sm: ms-[350px] sm: me-[350px]"  {...settings2}>
+            {/* food item -7 */}
+            <div
+              data-aos="flip-up"
+              className=" rounded-xl border-t-2 border-yellow-500 shadow-lg shadow-yellow-500 p-3 w-80 transition-transform transform hover:scale-105"
+            >
+              <img className="w-80 h-48 rounded-lg" src={photo7} alt="" />
+              <div>
+                <h1 className=" text-xl text-white font-semibold uppercase mt-5">
+                  Rasta Pasta
+                </h1>
+                <h2 className="text-[#808080] font-semibold">
+                  Lorem ipsum dolor sit amet consectetur <br /> adipisicing
+                  elit. Iusto, vitae?
+                </h2>
+                <div className="flex justify-between mt-5">
+                  <p className="text-yellow-500 text-2xl font-mono font-bold">
+                    $10.00
+                  </p>
+                  <div className="flex">
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon2} alt="" />
+                  </div>
+                </div>
+                <button
+                  onClick={handleOrderNowClick}
+                  className="hover:bg-[#FFD700] hover:text-black
+                             bg-[#FFD700]  text-[#808080] border-black font-bold px-2  py-1 
+                             rounded-md mt-3 flex items-center gap-2"
+                >
+                  <span> Add to Cart </span>
+                  <span>
+                    {" "}
+                    <BsFillCartPlusFill />{" "}
+                  </span>
+                </button>
+              </div>
+            </div>
+
+            {/* food item -8 */}
+            <div
+              data-aos="flip-up"
+              className=" rounded-xl border-t-2 border-yellow-500 shadow-lg shadow-yellow-500 p-3 w-80 transition-transform transform hover:scale-105"
+            >
+              <img className="w-80 h-48 rounded-lg" src={photo8} alt="" />
+              <div>
+                <h1 className=" text-xl text-white font-semibold uppercase mt-5">
+                  Ratatouille
+                </h1>
+                <h2 className="text-[#808080] font-semibold">
+                  Lorem ipsum dolor sit amet consectetur <br /> adipisicing
+                  elit. Iusto, vitae?
+                </h2>
+                <div className="flex justify-between mt-5">
+                  <p className="text-yellow-500 text-2xl font-mono font-bold">
+                    $09.80
+                  </p>
+                  <div className="flex">
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon2} alt="" />
+                  </div>
+                </div>
+                <button
+                  onClick={handleOrderNowClick}
+                  className="hover:bg-[#FFD700] hover:text-black
+                             bg-[#FFD700]  text-[#808080] border-black font-bold px-2  py-1 
+                             rounded-md mt-3 flex items-center gap-2"
+                >
+                  <span> Add to Cart </span>
+                  <span>
+                    {" "}
+                    <BsFillCartPlusFill />{" "}
+                  </span>
+                </button>
+              </div>
+            </div>
+
+            {/* food item -9 */}
+            <div
+              data-aos="flip-up"
+              className=" rounded-xl border-t-2 border-yellow-500 shadow-lg shadow-yellow-500 p-3 w-80 transition-transform transform hover:scale-105"
+            >
+              <img className="w-80 h-48 rounded-lg" src={photo1} alt="" />
+              <div>
+                <h1 className=" text-xl text-white font-semibold uppercase mt-5">
+                  Beef Wellington
+                </h1>
+                <h2 className="text-[#808080] font-semibold">
+                  Lorem ipsum dolor sit amet consectetur <br /> adipisicing
+                  elit. Iusto, vitae?
+                </h2>
+                <div className="flex justify-between mt-5">
+                  <p className="text-yellow-500 text-2xl font-mono font-bold">
+                    $22.50
+                  </p>
+                  <div className="flex">
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon2} alt="" />
+                  </div>
+                </div>
+                <button
+                  onClick={handleOrderNowClick}
+                  className="hover:bg-[#FFD700] hover:text-black
+                             bg-[#FFD700]  text-[#808080] border-black font-bold px-2  py-1 
+                             rounded-md mt-3 flex items-center gap-2"
+                >
+                  <span> Add to Cart </span>
+                  <span>
+                    {" "}
+                    <BsFillCartPlusFill />{" "}
+                  </span>
+                </button>
+              </div>
+            </div>
+            {/* food item -10 */}
+            <div
+              data-aos="flip-up"
+              className=" rounded-xl border-t-2 border-yellow-500 shadow-lg shadow-yellow-500 p-3 w-80 transition-transform transform hover:scale-105"
+            >
+              <img className="w-80 h-48 rounded-lg" src={photo1} alt="" />
+              <div>
+                <h1 className=" text-xl text-white font-semibold uppercase mt-5">
+                  Beef Wellington
+                </h1>
+                <h2 className="text-[#808080] font-semibold">
+                  Lorem ipsum dolor sit amet consectetur <br /> adipisicing
+                  elit. Iusto, vitae?
+                </h2>
+                <div className="flex justify-between mt-5">
+                  <p className="text-yellow-500 text-2xl font-mono font-bold">
+                    $22.50
+                  </p>
+                  <div className="flex">
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon1} alt="" />
+                    <img src={icon2} alt="" />
+                  </div>
+                </div>
+                <button
+                  onClick={handleOrderNowClick}
+                  className="hover:bg-[#FFD700] hover:text-black
+                             bg-[#FFD700]  text-[#808080] border-black font-bold px-2  py-1 
+                             rounded-md mt-3 flex items-center gap-2"
+                >
+                  <span> Add to Cart </span>
+                  <span>
+                    {" "}
+                    <BsFillCartPlusFill />{" "}
+                  </span>
+                </button>
+              </div>
+            </div>
+          </Slider>
 
         </div>
       </div>
