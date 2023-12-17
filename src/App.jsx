bb0e7b567c00dfb574b839c66a8b2784e33eb49c
git config --global user.name tamzid620 <tamzid620@gmail.com>
2023-12-17 12:19:56 +0600

    special menu background Banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import AdminPanel from "./Components/Pages/AdminPanel/AdminPanel";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/dp" element={<AdminPanel />} />
-        {/* Other routes */}
-      </Routes>
     </>
   );
 }
